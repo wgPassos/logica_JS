cc7a65f4b0610b454acb3496b0783d97f6476ef5
wgPassos <passoswilliangabriel@gmail.com>
2021-07-20 10:13:53 -0300

    New exercise

---
+++ aula_exer9/script.js
@@ -0,0 +1,47 @@
+// Pedir para usuário digitar um número de 1 a 6
+// Mostrar na tela uma mensagem difierente para 
+// cada número
+
+let numX = parseInt(prompt(`digite um número de 1 a 6:`));
+
+switch(numX) {
+    case 1:
+        alert(`Escolheu ${numX}`);
+        break;
+    case 2: 
+        alert(`Escolheu ${numX}`);
+        break;
+    case 3:
+        alert(`Escolheu ${numX}`);
+        break;
+    case 4: 
+        alert(`Escolheu ${numX}`);
+        break;
+    case 5: 
+        alert(`Escolheu ${numX}`);
+        break;
+    case 6: 
+        alert(`Escolheu ${numX}`);
+        break;
+    default:
+        alert(`Escolha de 1 a 6!`);
+}
+
+// if (numX === 1) {
+//     alert(`Você escolheu ${numX}`);
+// }
+// else if (numX === 2) {
+//     alert(`Você escolheu ${numX}`);
+// }
+// else if (numX === 3) {
+//     alert(`Você escolheu ${numX}`);
+// }
+// else if (numX === 4) {
+//     alert(`Você escolheu ${numX}`);
+// }
+// else if (numX === 5) {
+//     alert(`Você escolheu ${numX}`);
+// }
+// else if (numX === 6) {
+//     alert(`Você escolheu ${numX}`);
+// }
