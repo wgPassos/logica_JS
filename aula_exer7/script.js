ccd648291b2bd021456b47e0c99aa209ea8c4d92
wgPassos <passoswilliangabriel@gmail.com>
2021-07-21 11:21:55 -0300

    new exercise 21-07

--- aula_exer7/script.js
+++ aula_exer7/script.js
@@ -13,14 +13,43 @@
 // quem escolher o numero maior
 
 
+// let jog1 = parseInt(prompt(`Enter a number:`))
+// let jog2 = parseInt(prompt(`Enter another number:`));
+// let sorteio = parseInt(Math.random() * 2);
+
+// if (jog1 === jog2) {
+//     alert(`EMPATE! Número jogado foi: ${jog1}`);
+// }
+// else {
+//     if (sorteio == 0) {
+//         if (jog1 < jog2) {
+//             alert(`SORTEIO: ${sorteio}\nJOgador 1 ganhou,  com número menor: ${jog1}`);
+//         } else {
+//             alert(`SORTEIO: ${sorteio}\nO jogador 2 ganhou com número menor: ${jog2}`);
+//         }
+//     }else {
+//         if (jog1 > jog2) {
+//             alert(`SORTEIO: ${sorteio}\nO jogador 1 ganhou,  com número maior: ${jog1}`);
+//         }
+//         else {
+//             alert(`SORTEIO: ${sorteio}\nO jogador 2 ganhou, com número maior: ${jog2}`);
+//         }
+//     }
+// }
+
+
+
+// OU colocando o sorteio no comando de descisão
+// Dentro do else
+
 let jog1 = parseInt(prompt(`Enter a number:`))
 let jog2 = parseInt(prompt(`Enter another number:`));
-let sorteio = parseInt(Math.random() * 2);
 
 if (jog1 === jog2) {
     alert(`EMPATE! Número jogado foi: ${jog1}`);
 }
 else {
+    let sorteio = parseInt(Math.random() * 2);
     if (sorteio == 0) {
         if (jog1 < jog2) {
             alert(`SORTEIO: ${sorteio}\nJOgador 1 ganhou,  com número menor: ${jog1}`);
