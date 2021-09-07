0ef4058081c5e5525c3b71d872b72af7d5f15b18
wgPassos <passoswilliangabriel@gmail.com>
2021-09-07 09:20:00 -0300

    Exerc Function 01

---
+++ 02-funcoes/class_exer02/script.js
@@ -0,0 +1,16 @@
+var output = document.querySelector(".output");
+
+
+
+// var msg = parseInt(prompt(`Digite a tabuada desejada:`))
+
+// for (var i = 0; i < 100; i++) {
+//     msg += `i: ${msg} <br>`;
+    
+// }
+// output.innerHTML = msg;
+
+
+function mostrarTabuada () {
+    alert(`Mostrar a Tabuada`);
+}
