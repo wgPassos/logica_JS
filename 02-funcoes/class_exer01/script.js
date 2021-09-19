1980ae18bf39900a779856f04785c6387ff8e7b9
wgPassos <passoswilliangabriel@gmail.com>
2021-09-19 08:08:37 -0300

    melhorando funções

--- 02-funcoes/class_exer01/script.js
+++ 02-funcoes/class_exer01/script.js
@@ -3,21 +3,37 @@
 // Enquanto índice <= 1000
     // Concatena na variável
 // Mostra a msg no output
+var input = document.querySelector("#input")
 var output = document.querySelector(".output");
 var mensagem = "";
 
 function mostrarTabuada () {
-    var n = parseFloat(prompt(`Digite um número:`));
-    limparTabuada();
-    var i = 1;
-    while (i <= 100) {
-        mensagem += `${n} * ${i} = ${n * i}<br> `;
-        i++;
+    if (input.value === "") {
+        alert(`Digite algum número no campo`)
+        limparTabuada();
+    } else {
+
+        var n = parseFloat(input.value);
+        limparTabuada();
+        var i = 1;
+        while (i <= 100) {
+            mensagem += `${n} * ${i} = ${n * i}<br> `;
+            i++;
+        }
+        output.innerHTML = mensagem;
     }
-    output.innerHTML = mensagem;
 }
 
 function limparTabuada () {
     mensagem = "";
     output.innerHTML = mensagem;
+    input.value = "";
+}
+
+input.addEventListener("keypress", mostrarTabuadaEnter);
+
+function mostrarTabuadaEnter () {
+    if (event.which === 13) {
+        mostrarTabuada();
+    }
 }
