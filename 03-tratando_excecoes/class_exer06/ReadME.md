11e12d18203a5129b24fb57d219a7fda9b21cf21
wgPassos <passoswilliangabriel@gmail.com>
2021-09-24 23:59:07 -0300

    try catch

---
+++ 03-tratando_excecoes/class_exer06/ReadME.md
@@ -0,0 +1,13 @@
+## Problema ##
+
+Pedir para usuário digitar dois números que representam os lados de um retângulo assim que a página é carregada.
+
+Mostrar na tela a área total de duas formas distintas:
+
+- Num alert ou na div #output(dependendo do botão clicado).
+
+Esse código não precisa ser executado quando a página é aberta.
+
+### Sacada do exercício ###
+
+Foi criada uma função que faz calculo e retorna o resultado da área, assim a função pode ser utilizada em outras - reaproveitamento da função
---
+++ 03-tratando_excecoes/class_exer06/script.js
@@ -0,0 +1,29 @@
+
+function mostrarAreaAlert() {
+    var largura = parseFloat(prompt("Digite um numero:"));
+    var altura = parseFloat(prompt("Digite outro numero:"));
+
+    alert(`A área total é: ${calcularArea(largura, altura)}`);
+}
+
+function mostrarAreaOutput() {
+    var largura = parseFloat(prompt("Digite um numero:"));
+    var altura = parseFloat(prompt("Digite outro numero:"));
+
+    try {
+        document.querySelector("#output").innerHTML = `A área total é: ${calcularArea(largura, altura)}`;
+    } catch(e) {
+        alert(e);
+    }
+    
+}
+
+function calcularArea (l, a) {
+    if (l === undefined || a === undefined) {
+        throw new Error("calcularArea soh com numeros")
+    }
+    if (isNaN(l) || isNaN(a)) {
+        throw new Error("Apenas numeros")
+    }
+    return l * a
+}
